c27098afcedd86566086f122d4fe94fb56078f34
arry <arry.pandowo@gmail.com>
2024-03-26 15:07:01 +0700

    Initial commit

---
+++ src/main/java/id/co/madmink/encryptionservice/EncryptionApplication.java
@@ -0,0 +1,13 @@
+package id.co.madmink.encryptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptionApplication.class, args);
+    }
+
+}
